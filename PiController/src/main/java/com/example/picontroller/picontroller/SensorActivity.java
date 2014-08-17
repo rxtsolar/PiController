d8b03f1865c98bf957c9aa25bbc9140cc665bb0c
Guangyu Sun <guangyu.sun@oracle.com>
2014-08-16 21:13:55 -0700

    SensorActivity: detect tilt and send different message
    Signed-off-by: Guangyu Sun <guangyu.sun@oracle.com>

--- PiController/src/main/java/com/example/picontroller/picontroller/SensorActivity.java
+++ PiController/src/main/java/com/example/picontroller/picontroller/SensorActivity.java
@@ -7,7 +7,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -25,6 +24,7 @@ public class SensorActivity extends Activity implements SensorEventListener {
     private Socket socket;
     private static final int SERVER_PORT = 25640;
     private static final String SERVER_IP = "192.168.0.109";
+    private static final double THRESHOLD = 2.0;
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
@@ -35,24 +35,24 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
         if (mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null) {
             List<Sensor> gravitySensors = mSensorManager.getSensorList(Sensor.TYPE_GRAVITY);
-            Log.e("sensors", "you have " + gravitySensors.size() + " sensors");
+//            Log.e("sensors", "you have " + gravitySensors.size() + " sensors");
             for (int i = 0; i < gravitySensors.size(); i++) {
                 if ((gravitySensors.get(i).getVendor().contains("Google Inc.")) &&
                         (gravitySensors.get(i).getVersion() == 3)) {
-                    Log.e("sensors", "you chose number " + i);
+//                    Log.e("sensors", "you chose number " + i);
                     mGravity = gravitySensors.get(i);
                 }
             }
             if (mGravity == null) {
-                Log.e("sensors", "you chose default number 0");
+//                Log.e("sensors", "you chose default number 0");
                 mGravity = gravitySensors.get(0);
             }
         } else {
             if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-                Log.e("sensors", "you don't have gravity sensor, use accelerometer");
+//                Log.e("sensors", "you don't have gravity sensor, use accelerometer");
                 mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             } else {
-                Log.e("sensors", "you don't have any accelerometer sensor");
+//                Log.e("sensors", "you don't have any accelerometer sensor");
             }
         }
 
@@ -78,19 +78,37 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        Log.e("event", "you got " + event.values.length + " events");
-        for (int i = 0; i < event.values.length; i++) {
-            Log.e("event", "value[" + i + "] is " + event.values[i]);
+//        Log.e("event", "you got " + event.values.length + " events");
+//        for (int i = 0; i < event.values.length; i++) {
+//            Log.e("event", "value[" + i + "] is " + event.values[i]);
+//        }
+        String output = "";
+        double x = event.values[0];
+        double y = event.values[1];
+
+        if (y > THRESHOLD) {
+            output += 's';
+        } else if (y < -THRESHOLD) {
+            output += 'w';
         }
-        try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-            out.println("miao");
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (x > THRESHOLD) {
+            output += 'a';
+        } else if (x < -THRESHOLD) {
+            output += 'd';
+        }
+
+        if (!output.equals("")) {
+            try {
+                PrintWriter out = new PrintWriter(new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream())), true);
+                out.println(output);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
